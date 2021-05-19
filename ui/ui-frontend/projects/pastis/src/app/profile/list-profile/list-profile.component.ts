48e0a3a307275d702fdf5b5d358881eb87730f17
descamps <descamps@cines.fr>
2021-05-19 16:50:43 +0200

    KDE - 19/05/2021 - Ajout de l'icone complexe vitamui-icon-complex-element-1-1 + préparation du bouton search profil

--- ui/ui-frontend/projects/pastis/src/app/profile/list-profile/list-profile.component.ts
+++ ui/ui-frontend/projects/pastis/src/app/profile/list-profile/list-profile.component.ts
@@ -1,25 +1,25 @@
 /*
-Copyright © CINES - Centre Informatique National pour l'Enseignement Supérieur (2020) 
+Copyright © CINES - Centre Informatique National pour l'Enseignement Supérieur (2020)
 
 [dad@cines.fr]
 
-This software is a computer program whose purpose is to provide 
-a web application to create, edit, import and export archive 
+This software is a computer program whose purpose is to provide
+a web application to create, edit, import and export archive
 profiles based on the french SEDA standard
 (https://redirect.francearchives.fr/seda/).
 
 
 This software is governed by the CeCILL-C  license under French law and
-abiding by the rules of distribution of free software.  You can  use, 
+abiding by the rules of distribution of free software.  You can  use,
 modify and/ or redistribute the software under the terms of the CeCILL-C
 license as circulated by CEA, CNRS and INRIA at the following URL
-"http://www.cecill.info". 
+"http://www.cecill.info".
 
 As a counterpart to the access to the source code and  rights to copy,
 modify and redistribute granted by the license, users are provided only
 with a limited warranty  and the software's author,  the holder of the
 economic rights,  and the successive licensors  have only  limited
-liability. 
+liability.
 
 In this respect, the user's attention is drawn to the risks associated
 with loading,  using,  modifying and/or developing or reproducing the
@@ -28,9 +28,9 @@ that may mean  that it is complicated to manipulate,  and  that  also
 therefore means  that it is reserved for developers  and  experienced
 professionals having in-depth computer knowledge. Users are therefore
 encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or 
-data to be ensured and,  more generally, to use and operate it in the 
-same conditions as regards security. 
+requirements in conditions enabling the security of their systems and/or
+data to be ensured and,  more generally, to use and operate it in the
+same conditions as regards security.
 
 The fact that you are presently reading this means that you have had
 knowledge of the CeCILL-C license and that you accept its terms.
@@ -57,7 +57,7 @@ export class ListProfileComponent implements OnInit {
 
   @Input()
   uploader: FileUploader = new FileUploader({url: URL});
-  
+
   fileToUpload: File = null;
 
   displayedColumns: string[] = ['type', "id","baseName","lastModified"]
@@ -80,7 +80,9 @@ export class ListProfileComponent implements OnInit {
 
   data = ["portail"]
 
-  constructor(private profileService: ProfileService,private fileService: FileService, 
+  search: string;
+
+  constructor(private profileService: ProfileService,private fileService: FileService,
     private ngxLoader:NgxUiLoaderService, private router:Router, private noticeService : NoticeService) { }
 
   ngOnInit() {
@@ -127,6 +129,10 @@ export class ListProfileComponent implements OnInit {
         }
       });
     }
+  }
+
+  public onSearchSubmit(search: string): void {
+    this.search = search;
   }
 
   isRowHovered(elementId: number) {
