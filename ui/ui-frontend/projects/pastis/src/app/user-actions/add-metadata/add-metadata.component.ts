8cf8c26f9982275ea47303085d0e9fe6d6b21b51
naji <naji@cines.fr>
2021-05-21 17:35:52 +0200

    feat: popup add metadata

--- ui/ui-frontend/projects/pastis/src/app/user-actions/add-metadata/add-metadata.component.ts
+++ ui/ui-frontend/projects/pastis/src/app/user-actions/add-metadata/add-metadata.component.ts
@@ -1,25 +1,25 @@
 /*
-Copyright © CINES - Centre Informatique National pour l'Enseignement Supérieur (2020) 
+Copyright © CINES - Centre Informatique National pour l'Enseignement Supérieur (2020)
 
 [dad@cines.fr]
 
-This software is a computer program whose purpose is to provide 
-a web application to create, edit, import and export archive 
+This software is a computer program whose purpose is to provide
+a web application to create, edit, import and export archive
 profiles based on the french SEDA standard
 (https://redirect.francearchives.fr/seda/).
 
 
 This software is governed by the CeCILL-C  license under French law and
-abiding by the rules of distribution of free software.  You can  use, 
+abiding by the rules of distribution of free software.  You can  use,
 modify and/ or redistribute the software under the terms of the CeCILL-C
 license as circulated by CEA, CNRS and INRIA at the following URL
-"http://www.cecill.info". 
+"http://www.cecill.info".
 
 As a counterpart to the access to the source code and  rights to copy,
 modify and redistribute granted by the license, users are provided only
 with a limited warranty  and the software's author,  the holder of the
 economic rights,  and the successive licensors  have only  limited
-liability. 
+liability.
 
 In this respect, the user's attention is drawn to the risks associated
 with loading,  using,  modifying and/or developing or reproducing the
@@ -28,9 +28,9 @@ that may mean  that it is complicated to manipulate,  and  that  also
 therefore means  that it is reserved for developers  and  experienced
 professionals having in-depth computer knowledge. Users are therefore
 encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or 
-data to be ensured and,  more generally, to use and operate it in the 
-same conditions as regards security. 
+requirements in conditions enabling the security of their systems and/or
+data to be ensured and,  more generally, to use and operate it in the
+same conditions as regards security.
 
 The fact that you are presently reading this means that you have had
 knowledge of the CeCILL-C license and that you accept its terms.
@@ -118,7 +118,7 @@ export class UserActionAddMetadataComponent implements OnInit {
     this.allowedChildrenNames.sort((a, b) => {
       return orderedNames.indexOf(a) - orderedNames.indexOf(b)
     })
-    this.addedItems.length > 0 ? this.atLeastOneIsSelected = true : this.atLeastOneIsSelected = false 
+    this.addedItems.length > 0 ? this.atLeastOneIsSelected = true : this.atLeastOneIsSelected = false
     this.upateButtonStatusAndDataToSend();
   }
 
@@ -128,7 +128,7 @@ export class UserActionAddMetadataComponent implements OnInit {
       if (sedaNode.Cardinality !== (SedaCardinalityConstants.zeroOrMore || SedaCardinalityConstants.oreOrMore)) {
         this.allowedChildrenNames = this.allowedChildrenNames.filter(name=> {return name !== elementName});
       }
-      this.addedItems.length > 0 ? this.atLeastOneIsSelected = true : this.atLeastOneIsSelected = false 
+      this.addedItems.length > 0 ? this.atLeastOneIsSelected = true : this.atLeastOneIsSelected = false
     this.upateButtonStatusAndDataToSend();
   }
 
@@ -155,10 +155,14 @@ export class UserActionAddMetadataComponent implements OnInit {
 
   onYesClick(): void {
     console.log("Clicked ok on dialog : %o" , this.selectedSedaNode);
-    
+
   }
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  public onSearchSubmit(search: string): void {
+    this.filterName = search;
   }
 
 }
