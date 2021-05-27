d4ad3bfcb63dcd4c03d5a4b8a6dc09e8827a92f3
naji <naji@sandbox23.cines.fr>
2021-05-27 08:39:34 +0200

    fix: change environment pastis

--- ui/ui-frontend/projects/pastis/src/environments/environment.rec.ts
+++ ui/ui-frontend/projects/pastis/src/environments/environment.rec.ts
@@ -39,5 +39,5 @@ import{IEnvironment} from './IEnvironment'
 
 export const environment: IEnvironment= {
   production: false,
-  apiServerUrl: "https://sandbox78-adm.cines.fr:8051",
+  apiServerUrl: "https://sandbox23-adm.cines.fr:8051",
 };
