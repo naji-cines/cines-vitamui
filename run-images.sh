8cc36f638515b549a928a53ce1070ff7dca95d87
naji <naji@sandbox23.cines.fr>
2021-05-27 07:59:47 +0200

    feat: dockerization vitamui

---
+++ run-images.sh
@@ -0,0 +1,21 @@
+(docker stop security-internal && docker rm security-internal) || echo 'pas de container à stopper'
+(docker stop iam-internal && docker rm iam-internal) || echo 'pas de container à stopper'
+(docker stop iam-external && docker rm iam-external) || echo 'pas de container à stopper'
+(docker stop cas-server && docker rm cas-server) || echo 'pas de container à stopper'
+(docker stop ui-identity && docker rm ui-identity) || echo 'pas de container à stopper'
+(docker stop ui-portal && docker rm ui-portal) || echo 'pas de container à stopper'
+(docker stop ui-pastis && docker rm ui-pastis) || echo 'pas de container à stopper'
+(docker stop front-pastis && docker rm front-pastis) || echo 'pas de container à stopper'
+(docker stop front-portal && docker rm front-portal) || echo 'pas de container à stopper'
+(docker stop front-identity && docker rm front-identity) || echo 'pas de container à stopper'
+
+docker run -d --network host --name security-internal -p "8084:8084" cines/vitamui-security-internal
+docker run -d --network host --name iam-internal -p "7083:7083" cines/vitamui-iam-internal
+docker run -d --network host --name iam-external -p "8083:8083" cines/vitamui-iam-external
+docker run -d --network host --name cas-server -p "8080:8080" cines/vitamui-cas
+docker run -d --network host --name ui-portal -p "9000:9000" cines/vitamui-ui-portal
+docker run -d --network host --name ui-identity -p "9001:9001" cines/vitamui-ui-identity
+docker run -d --network host --name ui-pastis -p "8051:8051" cines/vitamui-ui-pastis
+docker run -d --network host --name front-pastis -p "4251:80" cines/vitamui-front-pastis
+docker run -d --network host --name front-identity -p "4201:80" cines/vitamui-front-identity
+docker run -d --network host --name front-portal -p "4200:80" cines/vitamui-front-portal
